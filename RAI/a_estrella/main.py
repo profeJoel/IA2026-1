6b1123c998c4ac85546a820c98b450ee170d8be1
profeJoel <joel.torres@ulagos.cl>
2026-04-08 15:42:03 -0400

    a estrella

--- RAI/a_estrella/main.py
+++ RAI/a_estrella/main.py
@@ -7,14 +7,15 @@ if __name__ == "__main__":
     #mapa_1 = grafo("archivo_grafo.txt")
     #print(mapa_1)
 
-    #origen = nodo(-3,4, "Origen")
+    origen = nodo(-3,4, "Origen")
     #destino = nodo(3, -2, "Destino")
-    #destino = nodo(-1,-3, "Destino")
-    #experimento = a_estrella(origen, destino, "archivo_grafo.txt")
+    destino = nodo(-1,-3, "Destino")
+    experimento = a_estrella(origen, destino, "archivo_grafo.txt")
 
     
-    origen = nodo(8, 54, "Origen")
-    destino = nodo(22, 32, "Destino")
-    experimento = a_estrella(origen, destino, "100nodos-chatgpt.csv")
-    print(experimento.tablero)
+    # origen = nodo(8, 54, "Origen")
+    # destino = nodo(22, 32, "Destino")
+    # experimento = a_estrella(origen, destino, "100nodos-chatgpt.csv")
+    # print(experimento.tablero)
+    
     experimento.init()
