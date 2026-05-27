f840aa4cd37fe9d943cf96d10a1b46c64ebff85b
profeJoel <joel.torres@ulagos.cl>
2026-05-27 15:42:29 -0400

    Actualización de prolog

---
+++ RAI/LÓGICA/gatos.pl
@@ -0,0 +1,23 @@
+%Hechos
+gato(gato_con_botas).
+gato(don_gato).
+gato(garfield).
+gato(tom).
+gato(felix).
+gato(juanito).
+gato(gaturro).
+
+especie(siames).
+especie(angora).
+especie(esfinge).
+especie(callejero).
+especie(tuxido).
+
+mascota(gato_con_botas, callejero).
+mascota(don_gato, callejero).
+mascota(garfield, angora).
+mascota(tom, callejero).
+mascota(felix, tuxido).
+mascota(juanito, tuxido).
+mascota(gaturro, callejero).
+
